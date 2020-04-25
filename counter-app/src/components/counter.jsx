21c2beb4005fa444ecb10f745bdd318a8392cf96
tharindu97 <tharindukavishna@gmail.com>
2020-04-25 10:16:31 +0530

    1h-16

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0,
-        tags:['tag1', 'tag2', 'tag3']
+        count: 0
      };
+
+    handleIncrement = product =>{
+        this.setState({ count: this.state.count + 1 })
+    };
     render() {
         let classes = this.getBadgeClasses();
         return ( 
             <div>
                 <h1><span className={classes}>{this.formatCount()}</span></h1>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    { this.state.tags.map(tag=> <li key={tag}>{ tag }</li>) }
-                </ul>
+                <button onClick={() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
             </div>
          );
     }
